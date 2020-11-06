5dc219a0074decbcea4d89ebd44f1050f751abdf
Emma Fewer <emmafewer20@gmail.com>
2020-11-06 22:10:57 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -17,7 +17,7 @@ def consolidate_cart(cart)
   new_cart = cart.uniq
   i = 0
   while i < new_cart.length do
-    new_cart[i][:count] = cart.count(|i| new_cart[i].included_in?(cart))
+    new_cart[i][:count] = cart.count {|i| new_cart[i].included_in?(cart)}
     i += 1 
   end
   binding.pry
