a4dc4dba1fa2d396642b993df69a67916d6a60e8
Emma Fewer <emmafewer20@gmail.com>
2020-11-06 22:27:30 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -30,33 +30,3 @@ def consolidate_cart(cart)
 end
 
 
-=begin
-    i = 0
-  while i < cart.length do
-    cart[i][:count] = cart.count(i)
-    i += 1 
-  end
-  
-  
-    new_array = cart.uniq
-  i = 0
-  while i < new_array.length do
-    new_array[i][:count] = cart.count(include? new_array[i][:item])
-    i += 1 
-  end
-  
-  
-  
-   :count = Hash.new(0)
-names.each { |name| counts[name] += 1 }
-  new_cart = []
-  i = 0 
-  while i < cart.length do 
-    if new_cart =! i
-      new_cart[i][:count] += 1 
-    else 
-      new_cart << i 
-      new_cart[i]
-        end
- [:count] = 1 
-=end
