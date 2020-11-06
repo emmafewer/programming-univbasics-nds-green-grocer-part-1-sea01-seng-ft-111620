17ab19c24a9cca793054587857478b8f030184cc
Emma Fewer <emmafewer20@gmail.com>
2020-11-06 22:05:00 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -14,14 +14,11 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-      i = 0
+  i = 0
   while i < cart.length do
     cart[i][:count] = cart.count(i)
     i += 1 
   end
- [:count] = 1 
-  end
-  new_cart
   binding.pry
 end
 
@@ -40,4 +37,19 @@ end
     new_array[i][:count] = cart.count(include? new_array[i][:item])
     i += 1 
   end
+  
+  
+  
+   :count = Hash.new(0)
+names.each { |name| counts[name] += 1 }
+  new_cart = []
+  i = 0 
+  while i < cart.length do 
+    if new_cart =! i
+      new_cart[i][:count] += 1 
+    else 
+      new_cart << i 
+      new_cart[i]
+        end
+ [:count] = 1 
 =end
